0714467820777a29e1104c084b98c6b9c4a0b50d
wood.wu <kilokingw@gmail.com>
2025-02-06 14:27:19 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <div className="fixed inset-0 bg-black z-[9999]" />
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
